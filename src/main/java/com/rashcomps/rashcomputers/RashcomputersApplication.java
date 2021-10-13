741438247f0ed77099cd0428a55e429a1d7e123b
ntwari-egide <ntwariegide2@gmail.com>
2021-10-13 19:52:41 +0200

    ft(base): initial commit

---
+++ src/main/java/com/rashcomps/rashcomputers/RashcomputersApplication.java
@@ -0,0 +1,13 @@
+package com.rashcomps.rashcomputers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RashcomputersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RashcomputersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rashcomps/rashcomputers/RashcomputersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rashcomps.rashcomputers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RashcomputersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
