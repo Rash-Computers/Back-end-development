837bb4e255806754cd08e7dccf09d3d21d25feb4
ntwari-egide <ntwariegide2@gmail.com>
2021-10-14 18:59:07 +0200

    ft(user-service): implement user service

--- src/main/java/com/rashcomps/rashcomputers/services/UserService.java
+++ src/main/java/com/rashcomps/rashcomputers/services/UserService.java
@@ -1,5 +1,7 @@
 package com.rashcomps.rashcomputers.services;
 
+import com.rashcomps.rashcomputers.dtos.CreateUser;
+import com.rashcomps.rashcomputers.dtos.UpdateUser;
 import com.rashcomps.rashcomputers.models.User;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +14,9 @@ public interface UserService {
 
     public User findById(Long id);
 
-    public  User signupNewUser();
+    public  User signupNewUser(CreateUser newUser);
 
-    public User updateNewUser();
+    public User updateNewUser(Long id, UpdateUser updatedUser);
 
     public Boolean deleteProject(Long id);
 
